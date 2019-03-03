ea690f0ba64aa9e54f5eb10701ae2781a4a93fc2
FrolovVitalii <phoenixfvv@gmail.com>
2019-03-04 00:02:25 +0200

    HomeWork lesson 4

---
+++ src/lesson4/hw/WorkWithCollections.java
@@ -0,0 +1,47 @@
+package lesson4.hw;
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class WorkWithCollections {
+
+    public ArrayList<String> removeItem(String item, ArrayList<String> list){
+        if(list!=null && !list.isEmpty()){
+            if(list.contains(item)) {
+                list.remove(item);
+            }
+        }
+        return list;
+    }
+
+    public ArrayList<String> removeItem(int index, ArrayList<String> list){
+        if(list!=null && !list.isEmpty()){
+            if(index >=0 && index <= list.size())
+            list.remove(index);
+        }
+        return list;
+    }
+
+    public boolean isCollectionContains(ArrayList<String> list, String item){
+        if(list.contains(item)){
+            System.out.println("We can't find element: " +item);
+            return true;
+        }
+        else{
+            System.out.println("Collection contains item: " +item);
+            return false;
+        }
+    }
+
+    public ArrayList<String> sortCollection(ArrayList<String> list){
+        list.sort(Comparator.naturalOrder());
+        return list;
+    }
+
+    public void printToConsole(ArrayList<String> list){
+        for(String str : list){
+            System.out.println(str);
+        }
+    }
+}
